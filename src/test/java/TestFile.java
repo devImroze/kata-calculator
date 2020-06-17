ecf07a76a4b42bb87da1e6315a08d47f0e66ec81
itsimroze <imrozehossainmolla@yahoo.com>
2020-06-17 23:09:40 +0530

    Test case with two numbers

--- src/test/java/TestFile.java
+++ src/test/java/TestFile.java
@@ -4,9 +4,14 @@ import static org.junit.Assert.assertEquals;
 
 public class TestFile {
     @Test
-    public void emptyStringCase(){
+    public void emptyStringCase() {
         KataCalculator kataCalculator = new KataCalculator();
         assertEquals(0, kataCalculator.Add(""));
     }
 
+    @Test
+    public void testWithTwoNumbers() {
+        KataCalculator kataCalculator = new KataCalculator();
+        assertEquals(5, kataCalculator.Add("2,3"));
+    }
 }
